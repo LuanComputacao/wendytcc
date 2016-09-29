715a10ee9a52a2c32beada6a06f7281f0eebe992
LuanComputacao <eng.luanroger@gmail.com>
2016-09-28 22:34:27 -0300

    including database configurations; database scripts to create tables; reformat code;

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$datbase = "estoque";
-mysql_connect($host,$user,$password);
+$host = "wendy.app";
+$user = "homestead";
+$password = "secret";
+$datbase = "wendy";
+mysql_connect($host, $user, $password) or die(mysql_error());
 mysql_select_db($datbase);
 ?>
